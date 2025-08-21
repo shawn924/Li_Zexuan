dd7d8da27566867657b35683f744549dae368c9f
shawn924 <66893665+shawn924@users.noreply.github.com>
2025-08-21 19:21:04 +0700

    Delete git1/scripts directory

--- git1/scripts/cbs_data_extractor.py
+++
@@ -1,9 +0,0 @@
-# 示例：在scripts/extract.py中添加功能
-import requests
-
-def fetch_cbs_data():
-    """Fetch data from Dutch Central Bureau of Statistics API"""
-    response = requests.get("https://opendata.cbs.nl/ODataApi/odata/83710NED/TypedDataSet")
-    return response.json()
-
-data = fetch_cbs_data
